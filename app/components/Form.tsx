f06acd7adb7543a5c2ba9acc4056da9f53bf42b1
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2024-04-28 21:56:26 +0200

    chaged with

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -38,14 +38,12 @@ function SubmitButton() {
   return (
     <>
       {pending ? (
-        <Button disabled className="w-full">
+        <Button disabled>
           <Loader2 className="mr-2 h-4 w-4 animate-spin" />
           Please Wait
         </Button>
       ) : (
-        <Button className="w-full" type="submit">
-          Sign for free
-        </Button>
+        <Button type="submit">Sign for free</Button>
       )}
     </>
   );
